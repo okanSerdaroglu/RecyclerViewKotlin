7bdd2a1b8bacebe1eb6df06b8be14640cd23b3fb
i_serdaroglu@thy.com <okanserdaroglu2@gmail.com>
2019-11-20 10:58:29 +0300

    delete and copy functions

--- app/src/main/java/com/okanserdaroglu/recyclerviewkotlin/ViewAdapter.kt
+++ app/src/main/java/com/okanserdaroglu/recyclerviewkotlin/ViewAdapter.kt
@@ -19,7 +19,6 @@ class ViewAdapter(private var allViews: ArrayList<com.okanserdaroglu.recyclervie
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewViewHolder { // 1
 
         Log.e("RECYCLERVIEW", "onCreateViewHolder")
-
         val inflater = LayoutInflater.from(parent.context)
         val rowLayout = inflater.inflate(
             R.layout.adapter_main,
@@ -32,14 +31,34 @@ class ViewAdapter(private var allViews: ArrayList<com.okanserdaroglu.recyclervie
 
     override fun onBindViewHolder(holder: ViewViewHolder, position: Int) { // 3
 
+        holder.bind(position,allViews[position])
         Log.e("RECYCLERVIEW", "onBindViewHolder")
-        holder.textViewHeader.text = allViews[position].header
-        holder.textViewDetail.text = allViews[position].detail
-        holder.imageViewHeader.setImageResource(allViews[position].image)
 
     }
 
-    class ViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) { // 2
+    inner class ViewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) { // 2
+
+        fun bind(position: Int, view : com.okanserdaroglu.recyclerviewkotlin.View) {
+            textViewHeader.text = view.header
+            textViewDetail.text = view.detail
+            imageViewHeader.setImageResource(view.image)
+            imageViewDelete.setOnClickListener {
+
+                allViews.removeAt(position)
+                notifyItemRemoved(position)
+                notifyItemRangeChanged(position,allViews.size) // this method notifies new positions of elements in list
+
+
+            }
+
+            imageViewCopy.setOnClickListener {
+
+                allViews.add(position,view)
+                notifyItemInserted(position)
+                notifyItemRangeChanged(position,allViews.size) // this method notifies new positions of elements in list
+
+            }
+        }
 
         init {
             Log.e("RECYCLERVIEW", "ViewViewHolder")
@@ -50,6 +69,10 @@ class ViewAdapter(private var allViews: ArrayList<com.okanserdaroglu.recyclervie
         var textViewHeader = rowLayout.textViewHeader!!
         var textViewDetail = rowLayout.textViewDetail!!
         var imageViewHeader = rowLayout.imageViewView!!
+        var imageViewDelete = rowLayout.imageViewDelete!!
+        var imageViewCopy = rowLayout.imageViewCopy!!
+
+
 
     }
 }
